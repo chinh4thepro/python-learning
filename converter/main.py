17089a455d492bab8ee8017e08a874583b0ca91d
chinh4thepro <chinh4thepro@gmail.com>
2023-07-07 14:05:48 -0400

    about 30 minnutes into tutorial

--- converter/main.py
+++ converter/main.py
@@ -1,5 +1,27 @@
 import PySimpleGUI as sg
 
-layout = [ [sg.Text('Text')], [sg.Button('Button')], [sg.Input()] ]
+layout = [ 
+    [sg.Text('Text', enable_events = True, key = '-TEXT-'), sg.Spin(['item 1', 'item 2'])], 
+    [sg.Button('Button', key = 'Button1')], 
+    [sg.Input()],
+    [sg.Text('hello'), sg.Button('Test button', key = 'Button2')]
+]
 
-sg.Window('Converter',layout).read()
+window = sg.Window('Converter',layout)
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+
+    if event == 'Button1':
+        print('Button pressed')
+    
+    if event == 'Button2':
+        print('Test button pressed')
+
+    if event == '-TEXT-':
+        print('TEXt  pressedo')
+
+window.close()
